641a422c5e3e42be8974b743ceb4fb779e07a8f2
thearshadkhanzaee <zaeearshadkhan@gmail.com>
2022-08-06 18:32:26 +0530

    ourgwalior

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Header Scroll 
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+// swippper slide js
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
+
+
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 3000
+    })
+});
+
+
+
+
+
+
+
+
